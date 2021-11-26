7c3a08485ae450dbba656962eab186f71e096fb5
xennia316 <xennia316@gmail.com>
2021-11-26 02:46:37 +0100

    using react to display user information

---
+++ afritech/src/day08/users.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import User from './user';
+function Users(){
+    return(
+        <div>
+            
+            <User age="15">
+                Shekina
+            </User>
+            <User  age="0.2">
+                Salome
+            </User><
+                User age="35">
+                Rha'ah
+            </User>
+            <User age="18">
+               Sonia
+            </User>
+        </div>
+    )
+}
+export default Users;
+
+
